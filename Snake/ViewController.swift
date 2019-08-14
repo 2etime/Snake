ef01d65a9c35b82ccd47ae73e0bbd6bcb187c7d8
Rick Twohy <twohyjr@gmail.com>
2019-08-13 23:01:21 -0400

    Initial Commit

---
+++ Snake/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by Rick Twohy Jr on 8/13/19.
+//  Copyright © 2019 Tutorials. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
